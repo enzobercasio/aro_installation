df53ba32e32e8c6d7a2d1cdbd6f0c23c757c6e82
Lorenzo Bercasio <contact@enzobercasio.com>
2022-10-06 10:49:44 +0800

    Update 03vnet.tf

--- terraform/03vnet.tf
+++ terraform/03vnet.tf
@@ -10,13 +10,13 @@ module "arovnet" {
   vnet_name           = "aro-vnet"
   address_space       = ["10.0.0.0/22"]
   subnet_prefixes     = ["10.0.0.0/23", "10.0.2.0/23"]
-  subnet_names        = ["master-sub", "worker-sub"]
-  subnet_service_endpoints = {"master-sub" = ["Microsoft.ContainerRegistry"],
-                              "worker-sub" = ["Microsoft.ContainerRegistry"]
+  subnet_names        = ["master-subnet", "worker-subnet"]
+  subnet_service_endpoints = {"master-subnet" = ["Microsoft.ContainerRegistry"],
+                              "worker-subnet" = ["Microsoft.ContainerRegistry"]
                              }
   subnet_enforce_private_link_service_network_policies = {
-    "master-sub" = true,
-    "worker-sub" = true
+    "master-subnet" = true,
+    "worker-subnet" = true
   }
   tags                = {}
 }
